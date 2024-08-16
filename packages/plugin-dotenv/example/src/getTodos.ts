db0ccc91ff6fc1af15d7ee9f83ebc830c753cb91
neverland <chenjiahan.jait@bytedance.com>
2024-08-17 07:51:11 +0800

    fix: deprecate rspack-plugin-dotenv (#13)

--- packages/plugin-dotenv/example/src/getTodos.ts
+++
@@ -1,6 +0,0 @@
-export async function getTodos() {
-  console.log(process.env.API_URL);
-  const response = await fetch(process.env.API_URL ?? '');
-  const todos = await response.json();
-  return todos;
-}
